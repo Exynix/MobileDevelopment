9e75ee6edd09a6329bf9e0fb4688f1e21a2473c9
Exynix <davids_barretom@javeriana.edu.co>
2023-08-22 14:39:31 -0500

    Upload of all relevant folders.

---
+++ Week4/app/src/main/java/com/example/week4lab01/MainActivity.kt
@@ -0,0 +1,13 @@
+package com.example.week4lab01
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        
+    }
+}
